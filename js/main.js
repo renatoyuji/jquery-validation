0e483be7c967fd23b93c074f3c3b22cff5742a69
renato <renatotanaka2002@gmail.com>
2023-05-08 16:26:47 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: '<br> Requerido',
+            email: '<br> Requerido',
+            telefone: '<br> Requerido',
+            cpf: '<br> Requerido',
+            endereco: '<br> Requerido',
+            cep: '<br> Requerido'
+        },
+        submitHandler: function() {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            console.log(camposIncorretos)
+        }
+    })
+})
